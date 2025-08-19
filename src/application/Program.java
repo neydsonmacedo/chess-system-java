4d7fa3d65e5ee041f15012ea4a27b8a9f1e0f3ee
neydsonmacedo <neydson_macedo@hotmail.com>
2025-08-19 20:37:55 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,13 @@
 package application;
+
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args){
-        System.out.println("Project Created");
+        //System.out.println("Project Created");
+        Position pos = new Position(3,5);
+        System.out.println(pos);
+
 
     }
 }
